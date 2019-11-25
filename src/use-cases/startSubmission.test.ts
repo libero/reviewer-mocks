9918e15721505d88efbd1c4cd819e6c5bbdf32fd
Cory <c.donkin@elifesciences.org>
2019-11-25 16:55:14 +0000

    unit test start submission

---
+++ src/use-cases/startSubmission.test.ts
@@ -0,0 +1,9 @@
+import { startSubmission } from './startSubmission';
+
+describe('startSubmisison', (): void => {
+  it('creates a new submission', ():void => {
+    const submissions = [];
+    startSubmission(submissions)();
+    expect(submissions).toHaveLength(1);
+  });
+});
