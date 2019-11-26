14e7e17b02174cfb1978e287c9f5a2293d367deb
Cory <c.donkin@elifesciences.org>
2019-11-26 09:38:08 +0000

    add pre-commit hooks

--- src/use-cases/startSubmission.test.ts
+++ src/use-cases/startSubmission.test.ts
@@ -1,9 +1,9 @@
 import { startSubmission } from './startSubmission';
 
 describe('startSubmisison', (): void => {
-  it('creates a new submission', ():void => {
-    const submissions = [];
-    startSubmission(submissions)();
-    expect(submissions).toHaveLength(1);
-  });
+    it('creates a new submission', (): void => {
+        const submissions = [];
+        startSubmission(submissions)();
+        expect(submissions).toHaveLength(1);
+    });
 });
