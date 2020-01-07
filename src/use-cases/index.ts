85eeb36cb20b176835fca3720945d663780bb1e1
Cory <c.donkin@elifesciences.org>
2020-01-07 14:41:52 +0000

    add redirect location endpoint for use in integration tests

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -5,6 +5,7 @@ export * from './authenticate';
 export * from './getSubmissions';
 export * from './startSubmission';
 export * from './getProfile';
+export * from './redirectLocation';
 
 export const HealthCheck = () => (req: Request, res: Response): void => {
     res.json({ ok: true });
