5099d1611e165b416d38870189e8198d4b016148
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-01-31 17:51:21 +0000

    Update startSubmission stub to take article type param

--- src/use-cases/startSubmission.test.ts
+++ src/use-cases/startSubmission.test.ts
@@ -3,7 +3,7 @@ import { startSubmission } from './startSubmission';
 describe('startSubmisison', (): void => {
     it('creates a new submission', (): void => {
         const submissions = [];
-        startSubmission(submissions)();
+        startSubmission(submissions)('articleInput');
         expect(submissions).toHaveLength(1);
     });
 });
--- src/use-cases/startSubmission.ts
+++ src/use-cases/startSubmission.ts
@@ -1,10 +1,11 @@
 import { v4 } from 'uuid';
 
-export const startSubmission = (submissions): (() => {}) => () => {
+export const startSubmission = (submissions): ((articleType: string) => {}) => (articleType: string) => {
     const submission = {
         id: v4(),
         title: '',
         updated: new Date().toISOString(),
+        articleType,
     };
     submissions.push(submission);
     return submission;
