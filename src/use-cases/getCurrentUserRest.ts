23a3aef863ccc04ab24abe43ca979f825345ac3f
Will Byrne <w.byrne@elifesciences.org>
2020-02-12 16:36:16 +0000

    remove file accidentaly checked in

--- src/use-cases/getCurrentUserRest.ts
+++
@@ -1,8 +0,0 @@
-import getMockData from '../getMockData';
-import { Request, Response } from 'express';
-
-export const getCurrentUserRest = () => (req: Request, res: Response): void => {
-    const user = getMockData('currentUser.json');
-    console.log('user: ', user);
-    res.json(user);
-};
