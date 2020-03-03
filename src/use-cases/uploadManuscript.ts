91b1e85537094279d709df826eed6c9702a75aed
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-03 15:39:20 +0000

    Align with reviewer submission

--- src/use-cases/uploadManuscript.ts
+++ src/use-cases/uploadManuscript.ts
@@ -1,11 +1,33 @@
-export const uploadManuscript = (submissions): ((_, { id }) => string | undefined) => (
-    _,
-    { id },
-): string | undefined => {
+enum FileStatus {
+    CREATED = 'CREATED',
+    UPLOADED = 'UPLOADED',
+    STORED = 'STORED',
+    CANCELLED = 'CANCELLED',
+}
+
+enum FileType {
+    MANUSCRIPT_SOURCE_PENDING = 'MANUSCRIPT_SOURCE_PENDING',
+    MANUSCRIPT_SOURCE = 'MANUSCRIPT_SOURCE',
+    SUPPORTING_FILE = 'SUPPORTING_FILE',
+}
+
+export const uploadManuscript = (submissions): ((_, { id }) => string | undefined) => (_, { id }) => {
     const submissionIndex = submissions.findIndex(submission => submission.id === id);
     if (submissionIndex !== -1) {
-        submissions.splice(submissionIndex, 1);
-        return id;
+        return {
+            ...submissions[submissionIndex],
+            ...{
+                file: {
+                    id: '0ee77f61-b89d-40cd-893e-88089359eb6b',
+                    type: FileType.MANUSCRIPT_SOURCE,
+                    filename: 'somefile',
+                    url: 'http://localhost/bucket/name.pdf',
+                    mimeType: 'application/pdf',
+                    size: '1000',
+                    status: FileStatus.UPLOADED,
+                },
+            },
+        };
     }
 
     throw new Error("can't find submission: " + id);
