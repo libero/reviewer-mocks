5fca72570a00b8a746f06ea1905505e0b62f9df6
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-03 15:42:18 +0000

    Add types

--- src/use-cases/uploadManuscript.ts
+++ src/use-cases/uploadManuscript.ts
@@ -11,21 +11,32 @@ enum FileType {
     SUPPORTING_FILE = 'SUPPORTING_FILE',
 }
 
+type File = {
+    id: string;
+    type: string;
+    filename: string;
+    url: string;
+    mimeType: string;
+    size: number;
+    status: string;
+};
+
 export const uploadManuscript = (submissions): ((_, { id }) => string | undefined) => (_, { id }) => {
     const submissionIndex = submissions.findIndex(submission => submission.id === id);
     if (submissionIndex !== -1) {
+        const file: File = {
+            id: '0ee77f61-b89d-40cd-893e-88089359eb6b',
+            type: FileType.MANUSCRIPT_SOURCE,
+            filename: 'somefile',
+            url: 'http://localhost/bucket/name.pdf',
+            mimeType: 'application/pdf',
+            size: 1000,
+            status: FileStatus.UPLOADED,
+        };
         return {
             ...submissions[submissionIndex],
             ...{
-                file: {
-                    id: '0ee77f61-b89d-40cd-893e-88089359eb6b',
-                    type: FileType.MANUSCRIPT_SOURCE,
-                    filename: 'somefile',
-                    url: 'http://localhost/bucket/name.pdf',
-                    mimeType: 'application/pdf',
-                    size: '1000',
-                    status: FileStatus.UPLOADED,
-                },
+                file,
             },
         };
     }
