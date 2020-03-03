7db49d6f7a05adf94afdd36849f84d1d2b502d3d
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-03 14:20:59 +0000

    WIP

---
+++ src/use-cases/uploadManuscript.ts
@@ -0,0 +1,12 @@
+export const uploadManuscript = (submissions): ((_, { id }) => string | undefined) => (
+    _,
+    { id },
+): string | undefined => {
+    const submissionIndex = submissions.findIndex(submission => submission.id === id);
+    if (submissionIndex !== -1) {
+        submissions.splice(submissionIndex, 1);
+        return id;
+    }
+
+    throw new Error("can't find submission: " + id);
+};
