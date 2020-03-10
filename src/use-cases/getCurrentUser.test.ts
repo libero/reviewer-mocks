06d21469c175d49a33000805926b63593980fd94
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-10 11:31:25 +0000

    Fix jwt decoding + test

--- src/use-cases/getCurrentUser.test.ts
+++ src/use-cases/getCurrentUser.test.ts
@@ -1,4 +1,4 @@
-import { getCurrentUser } from './getCurrentUser';
+import { getCurrentUser, getSubFromAuthorizationHeader } from './getCurrentUser';
 
 describe('getCurrentUser', (): void => {
     it('returns a user', (): void => {
@@ -9,5 +9,12 @@ describe('getCurrentUser', (): void => {
         expect(typeof user.id).toBe('string');
         expect(typeof user.name).toBe('string');
         expect(typeof user.role).toBe('string');
+    });
+    it('it should return sub from valid header', (): void => {
+        const header =
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjMGU2NGE4Ni0yZmViLTQzNWQtYTQwZi0wMWY5MjAzMzRiYzQiLCJpYXQiOjE1ODM4Mzg5MTF9.wD0oDVUYzU65mnELonbR1q_bTBMYdwtksYH3Tbp4dJ0';
+        const expectedSub = 'c0e64a86-2feb-435d-a40f-01f920334bc4';
+        const sub = getSubFromAuthorizationHeader(header);
+        expect(sub).toBe(expectedSub);
     });
 });
