405488bbb854a7abfa1e2532600f5e285cc8f3de
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-03-10 11:35:10 +0000

    Fix jwt decoding and test + restore currentUser json

--- src/use-cases/getCurrentUser.ts
+++ src/use-cases/getCurrentUser.ts
@@ -5,12 +5,22 @@ export const getCurrentUser = (): object => {
     return getMockData('currentUser.json');
 };
 
+export const getSubFromAuthorizationHeader = (header: string): string => {
+    /* This routine doesn't validate the token. Just decodes it and returns the user id that
+       tests can request a different user than the default to test resource ownership.
+       @todo: is this repository needed in the long run?
+    */
+    const jwt = header.split(' ')[1];
+    const token = JSON.parse(Buffer.from(jwt.split('.')[1], 'base64').toString());
+    return token.sub;
+};
+
 export const getCurrentUserREST = () => (req: Request, res: Response): void => {
-    const header = req.headers.authorization;
-    if (typeof header !== 'undefined') {
+    const authorization = req.headers.authorization;
+    if (typeof authorization !== 'undefined') {
         try {
-            const token = JSON.parse(Buffer.from(header, 'base64').toString());
-            res.json({ ...getCurrentUser(), id: token.sub });
+            res.json({ ...getCurrentUser(), id: getSubFromAuthorizationHeader(authorization) });
+            return;
         } catch (e) {}
     }
     res.json(getCurrentUser());
