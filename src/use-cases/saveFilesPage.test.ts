1a2e127750eaac76fa5125f85ab56b6c099d76a2
Cory <c.donkin@elifesciences.org>
2020-03-24 11:20:54 +0000

    fix a filesis undefined bug

--- src/use-cases/saveFilesPage.test.ts
+++ src/use-cases/saveFilesPage.test.ts
@@ -27,4 +27,18 @@ describe('saveFilesPage', (): void => {
         expect(submissions).toHaveLength(1);
         expect(submissions[0].files.coverLetter).toBe('test');
     });
+    it('save coverLetter of a submission if it has no files property', (): void => {
+        const submissionId = v4();
+        const submissions = [
+            {
+                id: submissionId,
+            },
+        ];
+        saveFilesPage(submissions as Array<{ id: string; files: { coverLetter: string } }>)(null, {
+            id: submissionId,
+            coverLetter: 'test',
+        });
+        expect(submissions).toHaveLength(1);
+        expect((submissions[0] as { id: string; files: { coverLetter: string } }).files.coverLetter).toBe('test');
+    });
 });
