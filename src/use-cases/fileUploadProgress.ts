6b399d30519f8823480ab01ebf2321b9dcd5bdf0
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-03-27 09:48:14 +0000

    Leave comment on user id

--- src/use-cases/fileUploadProgress.ts
+++ src/use-cases/fileUploadProgress.ts
@@ -4,8 +4,9 @@ export const fileUploadProgress = (pubsub: PubSub): { subscribe: ResolverFn } =>
     subscribe: withFilter(
         () => pubsub.asyncIterator('UPLOAD_STATUS'),
         (payload, variables, context) => {
+            // Please note that mocks does not filter on user id as this context is never set.
+            // Reviewer-submssions does use the userid
             return (
-                // payload.fileUploadProgress.userId === context.userId &&
                 payload.fileUploadProgress.submissionId === variables.submissionId
             );
         },
