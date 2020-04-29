0b3d1013b0d0440c1847315e77d2217c2796664f
diversemix <diversemix@gmail.com>
2020-04-29 09:10:13 +0100

    Create a suggestion in the saveFilesPage (#77)

--- src/use-cases/saveFilesPage.test.ts
+++ src/use-cases/saveFilesPage.test.ts
@@ -1,18 +1,31 @@
 import { v4 } from 'uuid';
 import { saveFilesPage } from './saveFilesPage';
+import { MockSubmissionRepo } from './types';
 
 describe('saveFilesPage', (): void => {
     it('save files returns error if id not found', (): void => {
         const submissionId = v4();
-        const submissions = [
+        const submissions: MockSubmissionRepo = [
             {
                 id: v4(),
                 files: { coverLetter: '' },
+                suggestions: [],
             },
         ];
 
         const badRequest = (): object => saveFilesPage(submissions)(null, { id: submissionId, coverLetter: 'test' });
         expect(badRequest).toThrow();
+    });
+    it('save on files page produces a suggestion', (): void => {
+        const submissionId = v4();
+        const submissions: MockSubmissionRepo = [{ id: submissionId, files: { coverLetter: '' }, suggestions: [] }];
+        saveFilesPage(submissions)(null, { id: submissionId, coverLetter: 'test' });
+        expect(submissions).toHaveLength(1);
+        expect(submissions[0].suggestions).toHaveLength(1);
+        expect(submissions[0].suggestions[0]).toHaveProperty('fieldName');
+        expect(submissions[0].suggestions[0].fieldName).toBe('title');
+        expect(submissions[0].suggestions[0]).toHaveProperty('value');
+        expect(submissions[0].suggestions[0].value).toBe('My first Manuscript');
     });
     it('save coverLetter of a submission', (): void => {
         const submissionId = v4();
@@ -20,6 +33,7 @@ describe('saveFilesPage', (): void => {
             {
                 id: submissionId,
                 files: { coverLetter: '' },
+                suggestions: [],
             },
         ];
         expect(submissions[0].files.coverLetter).toBe('');
@@ -34,10 +48,11 @@ describe('saveFilesPage', (): void => {
                 id: submissionId,
             },
         ];
-        saveFilesPage(submissions as Array<{ id: string; files: { coverLetter: string } }>)(null, {
+        saveFilesPage(submissions as MockSubmissionRepo)(null, {
             id: submissionId,
             coverLetter: 'test',
         });
+
         expect(submissions).toHaveLength(1);
         expect((submissions[0] as { id: string; files: { coverLetter: string } }).files.coverLetter).toBe('test');
     });
