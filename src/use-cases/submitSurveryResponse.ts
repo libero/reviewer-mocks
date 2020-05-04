e45884b71ba088cab784a9bd9788a0b1b2a73dce
noris alsaadie <>
2020-05-04 16:13:14 +0100

    Align submission and mocks

--- src/use-cases/submitSurveryResponse.ts
+++ src/use-cases/submitSurveryResponse.ts
@@ -2,10 +2,10 @@ import { v4 } from 'uuid';
 
 import { SurveyResponse, SurveyAnswer } from './types';
 
-export const submitSurveyRespnse = (
+export const submitSurveyResponse = (
     _,
     args: { surveyId: string; submissionId: string; answers: SurveyAnswer[] },
-): {} => {
+): SurveyResponse => {
     const id = v4();
     const { surveyId, submissionId, answers } = args;
     const surveyResponse = new SurveyResponse(id, surveyId, submissionId);
