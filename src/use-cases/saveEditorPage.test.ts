437b6f3455a3da919f2eabd1302ae8e35e2f29bb
noris alsaadie <>
2020-05-05 12:50:13 +0100

    Add tests

---
+++ src/use-cases/saveEditorPage.test.ts
@@ -0,0 +1,75 @@
+import { v4 } from 'uuid';
+import { saveEditorPage } from './saveEditorPage';
+
+describe('saveEditorPage', (): void => {
+    it('save returns error if id not found', (): void => {
+        const submissionId = v4();
+        const submissions = [
+            {
+                id: v4(),
+                editorDetails: {
+                    suggestedSeniorEditors: [],
+                    opposedSeniorEditors: [],
+                    opposedSeniorEditorsReason: '',
+                    suggestedReviewingEditors: [],
+                    opposedReviewingEditors: [],
+                    opposedReviewingEditorsReason: '',
+                    suggestedReviewers: [],
+                    opposedReviewers: [],
+                    opposedReviewersReason: '',
+                },
+            },
+        ];
+
+        const input = {
+            suggestedSeniorEditors: ['123'],
+            opposedSeniorEditors: ['456'],
+            opposedSeniorEditorsReason: 'no reason',
+            suggestedReviewingEditors: ['789'],
+            opposedReviewingEditors: ['1011'],
+            opposedReviewingEditorsReason: 'maybe a reason',
+            suggestedReviewers: [{ name: 'name1', email: 'email1@elifesciences.org' }],
+            opposedReviewers: [{ name: 'name2', email: 'email2@elifesciences.org' }],
+            opposedReviewersReason: 'probably a reason',
+        };
+
+        const badRequest = (): object => saveEditorPage(submissions)(null, { id: submissionId, editorDetails: input });
+        expect(badRequest).toThrow();
+    });
+
+    it('save editor details of a submission', (): void => {
+        const submissionId = v4();
+        const submissions = [
+            {
+                id: submissionId,
+                editorDetails: {
+                    suggestedSeniorEditors: [],
+                    opposedSeniorEditors: [],
+                    opposedSeniorEditorsReason: '',
+                    suggestedReviewingEditors: [],
+                    opposedReviewingEditors: [],
+                    opposedReviewingEditorsReason: '',
+                    suggestedReviewers: [],
+                    opposedReviewers: [],
+                    opposedReviewersReason: '',
+                },
+            },
+        ];
+
+        const input = {
+            suggestedSeniorEditors: ['123'],
+            opposedSeniorEditors: ['456'],
+            opposedSeniorEditorsReason: 'no reason',
+            suggestedReviewingEditors: ['789'],
+            opposedReviewingEditors: ['1011'],
+            opposedReviewingEditorsReason: 'maybe a reason',
+            suggestedReviewers: [{ name: 'name1', email: 'email1@elifesciences.org' }],
+            opposedReviewers: [{ name: 'name2', email: 'email2@elifesciences.org' }],
+            opposedReviewersReason: 'probably a reason',
+        };
+
+        saveEditorPage(submissions)(null, { id: submissionId, editorDetails: input });
+        expect(submissions).toHaveLength(1);
+        expect(submissions[0].editorDetails).toEqual(input);
+    });
+});
