3139b914577360622bb4d98e9ba51cdaddc7838f
noris alsaadie <>
2020-05-05 11:30:17 +0100

    WIP add editor details

---
+++ src/use-cases/saveEditorPage.ts
@@ -0,0 +1,58 @@
+import { v4 } from 'uuid';
+import { saveAuthorPage } from './saveAuthorPage';
+
+describe('saveEditorPage', (): void => {
+    it('save details returns error if id not found', (): void => {
+        const submissionId = v4();
+        const submissions = [
+            {
+                id: v4(),
+                details: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    institution: '',
+                },
+            },
+        ];
+
+        const input = {
+            firstName: 'Bob',
+            lastName: 'Windsor',
+            email: 'bob@nowhere.com',
+            institution: 'eLife',
+        };
+
+        const badRequest = (): object => saveAuthorPage(submissions)(null, { id: submissionId, details: input });
+        expect(badRequest).toThrow();
+    });
+
+    it('save author details of a submission', (): void => {
+        const submissionId = v4();
+        const submissions = [
+            {
+                id: submissionId,
+                author: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    institution: '',
+                },
+            },
+        ];
+
+        const input = {
+            firstName: 'Bob',
+            lastName: 'Windsor',
+            email: 'bob@nowhere.com',
+            institution: 'eLife',
+        };
+
+        saveAuthorPage(submissions)(null, { id: submissionId, details: input });
+        expect(submissions).toHaveLength(1);
+        expect(submissions[0].author.firstName).toBe('Bob');
+        expect(submissions[0].author.lastName).toBe('Windsor');
+        expect(submissions[0].author.email).toBe('bob@nowhere.com');
+        expect(submissions[0].author.institution).toBe('eLife');
+    });
+});
