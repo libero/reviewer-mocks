6cbf040b4f129d9707b51e14938c8ac614618572
diversemix <diversemix@gmail.com>
2020-05-12 16:15:38 +0100

    Fix startSubmission to return the files attribute

--- src/use-cases/startSubmission.ts
+++ src/use-cases/startSubmission.ts
@@ -3,9 +3,13 @@ import { v4 } from 'uuid';
 export const startSubmission = (submissions): ((_, { articleType }) => {}) => (_, { articleType }): {} => {
     const submission = {
         id: v4(),
-        title: '',
         updated: new Date().toISOString(),
         articleType,
+        status: 'INITIAL',
+        createdBy: 'userId',
+        files: {
+            supportingFiles: [],
+        },
     };
     submissions.push(submission);
     return submission;
