23844e417a67dbfb4cf263dadfd30fb755aa939b
diversemix <diversemix@gmail.com>
2020-05-13 16:09:32 +0100

    Add getEditors to the resolvers (#84)

--- src/use-cases/getEditors.ts
+++ src/use-cases/getEditors.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import getMockData from '../getMockData';
 
-export const GetEditors = (role: string) => (req: Request, res: Response): void => {
+export const getEditors = (role: string) => (req: Request, res: Response): void => {
     let editors: object[] = [];
     if (role == 'seniorEditors') {
         editors = getMockData('seniorEditors.json') as Array<object>;
