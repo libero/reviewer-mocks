65400af7931c1e015c43c15e1209a23203c1975a
diversemix <diversemix@gmail.com>
2020-05-27 09:42:05 +0100

    Improve logging for fileUploadProgress

--- src/use-cases/fileUploadProgress.ts
+++ src/use-cases/fileUploadProgress.ts
@@ -4,10 +4,10 @@ import { logger } from '../logger';
 export const fileUploadProgress = (pubsub: PubSub): { subscribe: ResolverFn } => ({
     subscribe: withFilter(
         () => pubsub.asyncIterator('UPLOAD_STATUS'),
-        (payload, variables, context) => {
+        (payload, variables) => {
             // Please note that mocks does not filter on user id as this context is never set.
             // Reviewer-submssions does use the userid
-            logger.info(`fileUploadProgress(${payload}, ${variables}, ${context})`);
+            logger.info(`fileUploadProgress(${JSON.stringify(payload)})`);
 
             return payload.fileUploadProgress.submissionId === variables.submissionId;
         },
